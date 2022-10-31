8f090609619657d31d703e91851adde74a836dae
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2022-10-31 15:25:28 +0100

    Split TensorArray into a separate class.

--- seemps/state/__init__.py
+++ seemps/state/__init__.py
@@ -1,4 +1,5 @@
-from .mps import MPS, TensorArray, MPSSum
+from .array import TensorArray
+from .mps import MPS, MPSSum
 from .factories import product, GHZ, W, wavepacket, graph, AKLT, random, gaussian
 from .canonical_mps import CanonicalMPS
 from .truncation import DEFAULT_TOLERANCE
